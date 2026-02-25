6218c363ea4221837245bae8454cafe903de15f3
Alan Reis <alanreisanjo@gmail.com>
2026-02-24 22:26:52 -0300

    feat: add documentation configuration to CaffeineApiDocs

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,21 @@
 import openapi from "@elysiajs/openapi";
 import Elysia from "elysia";
+import type { OpenAPIV3 } from "openapi-types";
 
-export const CaffeineApiDocs = new Elysia().use(
-	openapi({
-		path: "/docs",
-		scalar: { showDeveloperTools: "never" },
-		enabled: process.env.NODE_ENV === "development",
-	}),
-);
+type Args = {
+	tags?: OpenAPIV3.TagObject[];
+	info?: OpenAPIV3.InfoObject;
+	servers?: OpenAPIV3.ServerObject[];
+	externalDocs?: OpenAPIV3.ExternalDocumentationObject;
+};
+
+export function CaffeineApiDocs(args: Args) {
+	return new Elysia().use(
+		openapi({
+			path: "/docs",
+			scalar: { showDeveloperTools: "never" },
+			enabled: process.env.NODE_ENV === "development",
+			documentation: args,
+		}),
+	);
+}
